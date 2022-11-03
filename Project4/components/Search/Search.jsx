1194821ccd4a8b7932dd017460524d910073e79e
Jacob Dankel <55810804+JacobDankel@users.noreply.github.com>
2022-11-03 15:40:39 -0700

    Added npm install to project 5

--- Project4/components/Search/Search.jsx
+++ Project4/components/Search/Search.jsx
@@ -9,10 +9,7 @@ const search = (string, items) => {
             valid.push(item);
         }
     });
-<<<<<<< Updated upstream
     console.log(string);
-=======
->>>>>>> Stashed changes
     return valid;
 };
 
@@ -22,19 +19,11 @@ export default class Search extends React.Component{
       super(props); // Must run the constructor of React.Component first
   
         this.state = {
-<<<<<<< Updated upstream
             defaultInputValue: this.props.defaultInputValue,
             searchItems: this.props.searchItems,
             searchCallbackFunction: this.props.searchCallback,
         };
         
-=======
-            searchCriteria: this.props.searchCriteria,
-            searchItems: this.props.seachItems,
-            searchCallBackFuntion: this.props.searchCallBackFuntion,
-        };
-
->>>>>>> Stashed changes
         this.handleChange = this.handleChange.bind(this);
     }
 
@@ -47,14 +36,7 @@ export default class Search extends React.Component{
         return (
             <div className="cse4050-course-search">
                 <label htmlFor="inId">Search: </label>
-<<<<<<< Updated upstream
-                <input type="text" value={this.state.defaultInputValue} onChange={this.handleChange}/>
-                <div className="cse4050-courses-input">
-                    Input Value: {this.state.defaultInputValue}
-                </div>
-=======
                 <input type="text" value={this.state.searchCriteria} onChange={this.handleChange}/>
->>>>>>> Stashed changes
             </div>
         );
     }
